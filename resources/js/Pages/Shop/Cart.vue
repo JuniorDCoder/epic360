816b2f765374ae1c4796b733a2e6ca7ec7326b5f
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-11-09 00:09:11 +0100

    feat: Made cart subtotal and total reactive

--- resources/js/Pages/Shop/Cart.vue
+++ resources/js/Pages/Shop/Cart.vue
@@ -2,27 +2,26 @@
     <BaseLayout>
         <div class="flex flex-col w-full py-4">
             <h1 class="pb-5 text-4xl font-semibold md:ml-24 text-primary-text">Cart</h1>
-            <div  v-if="count > 0">
-                <div data-aos="fade-up" data-aos-delay=300 data-aos-duration=600 v-for="product in cartItems" class="flex flex-col items-center justify-between gap-4 md:flex-row md:px-24">
+            <div v-if="count > 0">
+                <div data-aos="fade-up" data-aos-delay=300 data-aos-duration=600 v-for="product in cartItems" :key="product.rowId" class="flex flex-col items-center justify-between gap-4 md:flex-row md:px-24">
                     <img :src="`/storage/`+product.options.image" alt="Product" class="md:w-1/3">
                     <div class="flex flex-col md:gap-16 md:w-1/2">
                         <div class="flex flex-col items-end justify-between gap-6 md:items-center md:flex-row">
                             <span class="text-2xl font-semibold md:w-1/2 text-primary-text">{{product.name}}</span>
                             <div class="flex w-1/3 md:w-1/5 items-center justify-center md: gap-5 px-4 py-2.5 border border-gray-500 text-gray-700 rounded-sm">
-                                <span   @click="decrementQty(product)" class="text-2xl cursor-pointer select-none">-</span>
+                                <span @click="decrementQty(product)" class="text-2xl cursor-pointer select-none">-</span>
                                 <span class="text-2xl cursor-pointer">{{product.qty}}</span>
-                                <span @click="incrementQty(product)" class="text-2xl cursor-pointer select-none" >+</span>
+                                <span @click="incrementQty(product)" class="text-2xl cursor-pointer select-none">+</span>
                             </div>
                             <div class="flex flex-col items-end gap-2">
                                 <div class="flex gap-6">
-                                    <p v-if="product.options.actual_price" class="font-semibold text-gray-700 line-through text-md md:text-sm">€{{ product.options.actual_price* product.qty }}</p>
-                                    <p v-else class="text-gray-700 text-md md:text-smfont-semibold">€{{ product.price * product.qty }}</p>
-                                    <p v-if="product.options.actual_price !== product.price" class="font-semibold text-gray-700 text-md md:text-sm">€{{ product.price * product.qty}}</p>
+                                    <p v-if="product.options.actual_price" class="font-semibold text-gray-700 line-through text-md md:text-sm">€{{ product.options.actual_price * product.qty }}</p>
+                                    <p v-else class="font-semibold text-gray-700 text-md md:text-sm">€{{ product.price * product.qty }}</p>
+                                    <p v-if="product.options.actual_price !== product.price" class="font-semibold text-gray-700 text-md md:text-sm">€{{ product.price * product.qty }}</p>
                                 </div>
-                                <span class="text-primary-text">Save €{{(product.options.actual_price - product.price)* product.qty }}</span>
+                                <span class="text-primary-text">Save €{{ (product.options.actual_price - product.price) * product.qty }}</span>
                                 <span @click="removeCartItem(product)" class="font-semibold cursor-pointer text-md md:text-sm text-secondary">Remove</span>
                             </div>
-
                         </div>
                         <div class="flex flex-col">
                             <hr class="w-full my-4 border-gray-300" />
@@ -31,9 +30,7 @@
                                 <img src="/public/storage/icons/help.png" class="w-5 h-5" alt="Help" />
                             </div>
                         </div>
-
                     </div>
-
                 </div>
                 <div class="my-12 md:mx-24">
                     <hr class="mb-4 border-gray-300" />
@@ -54,16 +51,16 @@
                         <!-- Cart Totals -->
                         <div class="flex items-center justify-between w-full">
                             <span>Sub Total</span>
-                            <p>€2340</p>
+                            <p>€{{ subtotal }}</p>
                         </div>
                         <div class="flex items-center justify-between w-full">
                             <span>Shipping</span>
-                            <p>-€</p>
+                            <p>€{{ shipping }}</p>
                         </div>
                         <hr class="w-full border-gray-300" />
                         <div class="flex items-center justify-between w-full">
                             <span class="text-2xl font-semibold">Total</span>
-                            <p class="text-2xl font-semibold">€2340</p>
+                            <p class="text-2xl font-semibold">€{{ total }}</p>
                         </div>
                         <Button class="w-full" fill="secondary" textColor="white" :rounded="true">Proceed to Check Out</Button>
                         <Link :href="route('shop')" class="flex gap-3 text-sm text-secondary"><p><</p> Return to Shop Page</Link>
@@ -83,7 +80,6 @@
                 <LatestProducts :products="latestProducts"/>
             </div>
         </div>
-
     </BaseLayout>
 </template>
 
@@ -92,7 +88,7 @@ import BaseLayout from '@/Layouts/BaseLayout.vue'
 import Button from '../../Components/Button.vue';
 import { useForm, Link } from '@inertiajs/vue3';
 import { toast } from 'vue3-toastify';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { decrementCartCount } from '@/stores/cartStore';
 import LatestProducts from '../../Components/Sections/LatestProducts.vue';
 
@@ -101,7 +97,6 @@ const props = defineProps({
     count: Number,
     latestProducts: Array
 });
-
 
 const showCouponInput = ref(false);
 
@@ -133,6 +128,18 @@ const removeCartItem = (item) => {
         }
     });
 };
+
+const shipping = ref(0);
+
+const subtotal = computed(() => {
+    return Object.values(props.cartItems).reduce((sum, item) => {
+        return sum + (item.price * item.qty);
+    }, 0);
+});
+
+const total = computed(() => {
+    return subtotal.value + shipping.value;
+});
 </script>
 
 <style scoped>
